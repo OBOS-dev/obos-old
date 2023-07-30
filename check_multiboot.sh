a1903400c5a2cf08e9cd8c31a9cf2f14e3f25504
Omar Berrow <omarkberrow2000@outlook.com>
2023-07-30 13:54:03 -0400

    Remove all!

--- check_multiboot.sh
+++
@@ -1,6 +0,0 @@
-if grub-file --is-x86-multiboot out/oboskrnl; then
-    echo Found multiboot header in out/oboskrnl
-else
-    echo Something is wrong. "out/oboskrnl," has no multiboot header.
-    exit
-fi
--- find_addr.sh
+++
@@ -1,3 +0,0 @@
-export address=$@
-addr2line -e out/oboskrnl -f -p -i -r -a 0x$address
- objdump -d out/oboskrnl  -M intel | grep --color -C 10 -n $address
